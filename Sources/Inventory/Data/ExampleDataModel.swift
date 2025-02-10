156eee2d6e2c8c386a75274049d90ad81bf4952c
Ekam Bhatia <ekambhatia@Ekams-MacBook-Pro.local>
2025-02-09 19:57:31 -0500

    initial

---
+++ Sources/Inventory/Data/ExampleDataModel.swift
@@ -0,0 +1,62 @@
+//
+//  ExampleDataModel.swift
+//  Inventory
+//
+//  Created by Mohamed El-Halawani on 2025-02-03.
+//
+
+import Foundation
+import Hummingbird
+
+// This is an example data model object.
+struct ExampleDataModel {
+    let id: UUID
+    let name: String
+}
+
+
+// Part data model
+enum Category: String, Codable, Equatable{
+    case engine, transmission, brakes, body
+}
+
+struct Size: Codable, Equatable{
+    let height: Double
+    let width: Double
+    let length: Double
+}
+
+struct Part{
+    let id: UUID
+    let name: String
+    let category: Category
+    let size: Size
+    let weight: Double
+    
+}
+
+// Warehouse data model
+
+struct Location: Codable, Equatable{
+    let address: String
+    let city: String
+    let country: String
+}
+
+struct Warehouse{
+    let id: UUID
+    let name: String
+    let location: Location
+    let contactNumber: String
+    let manager: String
+    
+}
+
+
+
+// Every data model you create MUST contain this line (DON'T forget to change the object name)
+extension ExampleDataModel: ResponseEncodable, Decodable, Equatable {}
+
+extension Part: ResponseEncodable, Decodable, Equatable {}
+
+extension Warehouse: ResponseEncodable, Decodable, Equatable {}
